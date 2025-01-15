293f73f226d384fb25be627daf5d0e3b4b82dca3
Kass-S <kassiespacek101@gmail.com>
2025-01-15 13:18:31 -0800

    first commit

---
+++ Controllers/RockPaperScissorsLizardSpockController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RockPaperScissorLizardSpock.Services;
+
+namespace RockPaperScissorLizardSpock.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RockPaperScissorsLizardSpockController : ControllerBase
+    {
+        private readonly RockPaperScissorsLizardSpockServices _rockPaperScissorsLizardSpockServices;
+        public RockPaperScissorsLizardSpockController(RockPaperScissorsLizardSpockServices rockPaperScissorsLizardSpockServices){
+            _rockPaperScissorsLizardSpockServices = rockPaperScissorsLizardSpockServices;
+        }
+
+        [HttpGet]
+        [Route("PlayRPSLS")]
+        public string PlayGame(string userChoice)
+        {
+            return _rockPaperScissorsLizardSpockServices.PlayGame(userChoice);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Kassandra Spacek
+1/15/25
+
+Created an enpoint that will give you a CPU response.
+Prototype Figma approved by Bryan.
+
+Reviewed by: 
